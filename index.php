e633660bcf1b6db0f81cd11f6fcbc6d9a4306801
Tarik Muharem <tare98muharem@gmail.com>
2019-11-07 21:53:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,22 +9,7 @@ session_start();
 	<link rel="stylesheet" type="text/css" href="css/indexstyle.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
-           <script type="text/javascript">
-                var sel1 = document.querySelector('#sel1');
-                var sel2 = document.querySelector('#sel2');
-                var options2 = sel2.querySelectorAll('option');
-
-                function giveSelection(selValue) {
-                  sel2.innerHTML = '';
-                  for(var i = 0; i < options2.length; i++) {
-                    if(options2[i].dataset.option === selValue) {
-                      sel2.appendChild(options2[i]);
-                    }
-                  }
-                }
-
-                giveSelection(sel1.value);
-              </script>
+           
 </head>
 <body class="bg">
   <div class="header">
